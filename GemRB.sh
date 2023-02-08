5500b46776e7f73983b78f0501310e325df7223b
Jacob <kloptops@gmail.com>
2023-02-08 19:35:29 +0800

    First update
    - Fixed unzipping of engine files on first launch, it now copies over the demo files into the games directory, removed double up of demo data in port.
    - Added controls
    - Script now brings up a list of games when more than one is detected, it remembers the last game played.
    - Config files are copied into the game directory at launch, custom config files can be created for particular games.

--- GemRB.sh
+++ GemRB.sh
@@ -31,148 +31,175 @@ $ESUDO chmod 666 log.txt
 export TERM=linux
 printf "\033c" > $CUR_TTY
 
-# if [ -z ${GAME+x} ]; then
-
-#   printf "\e[?25h" > $CUR_TTY
-#   dialog --clear > $CUR_TTY
-
-
-#   $GPTOKEYB "dialog" -c "${GAMEDIR}/gemrb-menu.gptk" &
-
-#   GAMEEXES=( $(find games/ -maxdepth 2 -iname 'gameexe.ini' | sort) );
-#   if [ "${#GAMEEXES[@]}" -eq 0 ]; then
-#     dialog \
-#     --backtitle "Real Live VM" \
-#     --title "[ Error ]" \
-#     --clear \
-#     --msgbox "No Games Found!" $height $width > $CUR_TTY
-
-#     printf "\033c" > $CUR_TTY
-#     $ESUDO kill -9 $(pidof gptokeyb)
-#     $ESUDO systemctl restart oga_events &
-#     exit 1;
-#   fi
-
-#   VN_CHOICES=()
-#   VN_DIRS=()
-
-#   OFFSET=0
-#   if [ -f "${GAMEDIR}/save/last_game" ]; then
-#     LAST_GAME=$(cat "${GAMEDIR}/save/last_game")
-#     VN_CHOICES+=(0 "$LAST_GAME")
-#     VN_DIRS+=("$LAST_GAME")
-#     echo "0 -> **${LAST_GAME}**" 2>&1 | tee -a ./log.txt
-#     OFFSET=1
-#   else
-#     LAST_GAME=""
-#   fi
-
-#   for i in "${!GAMEEXES[@]}"; do
-#     VN_PATH=$(dirname "${GAMEEXES[$i]}")
-#     VN_DIR=${VN_PATH##*/}
-
-#     if [ "$VN_DIR" = "$LAST_GAME" ]; then
-#       OFFSET=$(($OFFSET-1))
-#     else
-#       i=$(($i+$OFFSET))
-#       echo "$i -> ${VN_DIR}" 2>&1 | tee -a ./log.txt
-#       VN_SHORTCUT="$PORTDIR/${VN_DIR}.sh"
-#       VN_DIRS+=("${VN_DIR}")
-#       if [ -f "$PORTDIR/${VN_DIR}.sh" ]; then
-#         VN_CHOICES+=($i "${VN_DIR} - Installed")
-#       else
-#         VN_CHOICES+=($i "${VN_DIR}")
-#       fi
-#     fi
-#   done
-
-#   IN_CHOICES=( "${VN_CHOICES[@]}" )
-
-#   VN_CHOICES+=("" "")
-#   VN_CHOICES+=("i" "Install Game Shortcut")
-
-#   if [ "${#VN_DIRS[@]}" -eq 1 ]; then
-#     GAME="${VN_DIRS[0]}"
-#   else
-#     GAME_SELECT=(dialog \
-#       --backtitle "Real Live VM" \
-#       --title "[ Select Game ]" \
-#       --clear \
-#       --menu "Choose Your Game" $height $width 15)
-
-
-#     VN_CHOICE=$("${GAME_SELECT[@]}" "${VN_CHOICES[@]}" 2>&1 > $CUR_TTY)
-#     if [ $? != 0 ]; then
-#       $ESUDO kill -9 $(pidof gptokeyb)
-#       $ESUDO systemctl restart oga_events &
-#       echo "QUIT: ${VN_CHOICE}" 2>&1 | $ESUDO tee -a ./log.txt
-#       printf "\033c" > $CUR_TTY
-#       exit 1;
-#     fi
-
-#     if [ $VN_CHOICE = "i" ]; then
-#       IN_SELECT=(dialog \
-#         --backtitle "Real Live VM" \
-#         --title "[ Install Game Shortcut ]" \
-#         --clear \
-#         --menu "Choose Your Game" $height $width 15)
-
-#       IN_CHOICE=$("${IN_SELECT[@]}" "${IN_CHOICES[@]}" 2>&1 > $CUR_TTY)
-#       if [ $? != 0 ]; then
-#         $ESUDO kill -9 $(pidof gptokeyb)
-#         $ESUDO systemctl restart oga_events &
-#         echo "QUIT: ${IN_CHOICE}" 2>&1 | tee -a ./log.txt
-#         printf "\033c" > $CUR_TTY
-#         exit 1;
-#       fi
-
-#       IN_GAME="${VN_DIRS[$IN_CHOICE]}"
-
-#       printf "#!/usr/bin/bash\nGAME=$IN_GAME $PORTDIR/Rlvm.sh\n" | $ESUDO tee "$PORTDIR/$IN_GAME.sh"
-
-#       dialog \
-#         --backtitle "Real Live VM" \
-#         --title "[ Success ]" \
-#         --clear \
-#         --msgbox "Installed shortcut for $IN_GAME\n\nRestart Emulation Station to find the game under ports." $height $width > $CUR_TTY
-
-#       $ESUDO kill -9 $(pidof gptokeyb)
-#       $ESUDO systemctl restart oga_events &
-#       printf "\033c" > $CUR_TTY
-#       exit 0;
-#     fi
-
-#     echo "C ${VN_CHOICE} -> ${VN_DIRS[$VN_CHOICE]}" 2>&1 | tee -a ./log.txt
-
-#     $ESUDO kill -9 $(pidof gptokeyb)
-
-#     GAME="${VN_DIRS[$VN_CHOICE]}"
-#   fi
-
-#   if [ ! -d "${GAMEDIR}/save" ]; then
-#     $ESUDO mkdir "${GAMEDIR}/save"
-#   fi
-#   # Save the selected game as the last game
-#   printf "$GAME" | $ESUDO tee "${GAMEDIR}/save/last_game"
-# fi
-
-if [ ! -d "$GAMEDIR/engine" ]; then
+if [ -f "$GAMEDIR/engine.zip" ]; then
+  if [ -d "$GAMEDIR/engine" ]; then
+    $ESUDO echo "Removing old engine."
+    $ESUDO rm -fRv "$GAMEDIR/engine"
+  fi
   # Extract the engine from the build zip.
-  unzip "$GAMEDIR/engine.zip"
-  mv -fv "$GAMEDIR/engine/gemrb" "$GAMEDIR/gemrb"
+  $ESUDO unzip "$GAMEDIR/engine.zip"
+  $ESUDO mv -fv "$GAMEDIR/engine/gemrb" "$GAMEDIR/gemrb"
+  $ESUDO rm -f "$GAMEDIR/engine.zip"
+fi
+
+if [ -d "$GAMEDIR/engine/demo" ]; then
+  # Copy demo over, but dont overwrite just incase it has been modified.
+  if [ -d "$GAMEDIR/games/demo" ]; then
+    $ESUDO rm -fRv "$GAMEDIR/engine/demo"
+  else
+    $ESUDO mv -fv "$GAMEDIR/engine/demo" "$GAMEDIR/games/demo"
+  fi
+fi
+
+if [ -z ${GAME+x} ]; then
+
+  printf "\e[?25h" > $CUR_TTY
+  dialog --clear > $CUR_TTY
+
+  $GPTOKEYB "dialog" -c "${GAMEDIR}/gemrb-menu.gptk" &
+
+  GAMEEXES=( $(find games/ -maxdepth 2 -iname 'chitin.key' | sort) );
+  if [ "${#GAMEEXES[@]}" -eq 0 ]; then
+    dialog \
+    --backtitle "Gem RB" \
+    --title "[ Error ]" \
+    --clear \
+    --msgbox "No Games Found!" $height $width > $CUR_TTY
+
+    printf "\033c" > $CUR_TTY
+    $ESUDO kill -9 $(pidof gptokeyb)
+    $ESUDO systemctl restart oga_events &
+    exit 1;
+  fi
+
+  VN_CHOICES=()
+  VN_DIRS=()
+
+  OFFSET=0
+  if [ -f "${GAMEDIR}/save/last_game" ]; then
+    LAST_GAME=$(cat "${GAMEDIR}/save/last_game")
+    VN_CHOICES+=(0 "$LAST_GAME")
+    VN_DIRS+=("$LAST_GAME")
+    echo "0 -> **${LAST_GAME}**" 2>&1 | tee -a ./log.txt
+    OFFSET=1
+  else
+    LAST_GAME=""
+  fi
+
+  for i in "${!GAMEEXES[@]}"; do
+    VN_PATH=$(dirname "${GAMEEXES[$i]}")
+    VN_DIR=${VN_PATH##*/}
+
+    if [ "$VN_DIR" = "$LAST_GAME" ]; then
+      OFFSET=$(($OFFSET-1))
+    else
+      i=$(($i+$OFFSET))
+      echo "$i -> ${VN_DIR}" 2>&1 | tee -a ./log.txt
+      VN_SHORTCUT="$PORTDIR/${VN_DIR}.sh"
+      VN_DIRS+=("${VN_DIR}")
+      if [ -f "$PORTDIR/${VN_DIR}.sh" ]; then
+        VN_CHOICES+=($i "${VN_DIR} - Installed")
+      else
+        VN_CHOICES+=($i "${VN_DIR}")
+      fi
+    fi
+  done
+
+  IN_CHOICES=( "${VN_CHOICES[@]}" )
+
+  VN_CHOICES+=("" "")
+  VN_CHOICES+=("i" "Install Game Shortcut")
+
+  if [ "${#VN_DIRS[@]}" -eq 1 ]; then
+    GAME="${VN_DIRS[0]}"
+  else
+    GAME_SELECT=(dialog \
+      --backtitle "Gem RB" \
+      --title "[ Select Game ]" \
+      --clear \
+      --menu "Choose Your Game" $height $width 15)
+
+
+    VN_CHOICE=$("${GAME_SELECT[@]}" "${VN_CHOICES[@]}" 2>&1 > $CUR_TTY)
+    if [ $? != 0 ]; then
+      $ESUDO kill -9 $(pidof gptokeyb)
+      $ESUDO systemctl restart oga_events &
+      echo "QUIT: ${VN_CHOICE}" 2>&1 | $ESUDO tee -a ./log.txt
+      printf "\033c" > $CUR_TTY
+      exit 1;
+    fi
+
+    if [ $VN_CHOICE = "i" ]; then
+      IN_SELECT=(dialog \
+        --backtitle "Gem RB" \
+        --title "[ Install Game Shortcut ]" \
+        --clear \
+        --menu "Choose Your Game" $height $width 15)
+
+      IN_CHOICE=$("${IN_SELECT[@]}" "${IN_CHOICES[@]}" 2>&1 > $CUR_TTY)
+      if [ $? != 0 ]; then
+        $ESUDO kill -9 $(pidof gptokeyb)
+        $ESUDO systemctl restart oga_events &
+        echo "QUIT: ${IN_CHOICE}" 2>&1 | tee -a ./log.txt
+        printf "\033c" > $CUR_TTY
+        exit 1;
+      fi
+
+      IN_GAME="${VN_DIRS[$IN_CHOICE]}"
+
+      printf "#!/usr/bin/bash\nGAME=$IN_GAME $PORTDIR/GemRB.sh\n" | $ESUDO tee "$PORTDIR/$IN_GAME.sh"
+
+      dialog \
+        --backtitle "Gem RB" \
+        --title "[ Success ]" \
+        --clear \
+        --msgbox "Installed shortcut for $IN_GAME\n\nRestart Emulation Station to find the game under ports." $height $width > $CUR_TTY
+
+      $ESUDO kill -9 $(pidof gptokeyb)
+      $ESUDO systemctl restart oga_events &
+      printf "\033c" > $CUR_TTY
+      exit 0;
+    fi
+
+    echo "C ${VN_CHOICE} -> ${VN_DIRS[$VN_CHOICE]}" 2>&1 | tee -a ./log.txt
+
+    $ESUDO kill -9 $(pidof gptokeyb)
+
+    GAME="${VN_DIRS[$VN_CHOICE]}"
+  fi
+
+  if [ ! -d "${GAMEDIR}/save" ]; then
+    $ESUDO mkdir "${GAMEDIR}/save"
+  fi
+  # Save the selected game as the last game
+  printf "$GAME" | $ESUDO tee "${GAMEDIR}/save/last_game"
 fi
 
 printf "\033c" > $CUR_TTY
 ## RUN SCRIPT HERE
 
+# Install appropriate GemRB.cfg
+GAMELC=$(echo "$GAME" | tr '[:upper:]' '[:lower:]')
+if [ ! -f "${GAMEDIR}/games/${GAME}/GemRB.cfg" ]; then
+  if [ -f "${GAMEDIR}/configs/GemRB.cfg.${GAMELC}" ]; then
+    # Does one for this game exist?
+    $ESUDO cp -v "${GAMEDIR}/configs/GemRB.cfg.${GAMELC}" "${GAMEDIR}/games/${GAME}/GemRB.cfg"
+  else
+    # Otherwise use the default one
+    $ESUDO cp -v "${GAMEDIR}/configs/GemRB.cfg.default" "${GAMEDIR}/games/${GAME}/GemRB.cfg"
+  fi
+fi
+
+export TEXTINPUTPRESET="Name"
+export TEXTINPUTINTERACTIVE="Y"
+export TEXTINPUTNOAUTOCAPITALS="Y"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
 export PYTHONHOME="$GAMEDIR"
+export LD_LIBRARY_PATH="$GAMEDIR/lib:$LD_LIBRARY_PATH"
 
-GAME="demo"
-
-$GPTOKEYB "gemrb" -c "${GAMEDIR}/gemrb.gptk" &
-$TASKSET ./gemrb $MSGOTHIC "${GAMEDIR}/games/${GAME}/" 2>&1 | $ESUDO tee -a ./log.txt
+$GPTOKEYB "gemrb" -c "${GAMEDIR}/gemrb.gptk" textinput &
+$TASKSET ./gemrb "${GAMEDIR}/games/${GAME}/" 2>&1 | $ESUDO tee -a ./log.txt
 
 $ESUDO kill -9 $(pidof gptokeyb)
 unset LD_LIBRARY_PATH
--- README.md
+++ README.md
@@ -6,9 +6,27 @@ This is the configuration files and the required steps to build the GemRB engine
 
 # Installation
 
-Use portmaster to install gemrb, copy the desired game or games into `{PORTFOLDER}/gemrb/games/{GAMENAME}`. On launch it will ask you to choose the game if it detects multiple games.
+Use portmaster to install gemrb, copy the desired game or games into `{PORTFOLDER}/gemrb/games/{GAME_ID}`. On launch it will ask you to choose the game if it detects multiple games.
 
 # Controls
+
+- R1/A: Left Click
+- L1/B: Right Click
+- L2: Priest spells
+- R2: Wizard spells
+- X: Inventory
+- Y: Character Record
+- Select+L1: Quick Save
+- Start: pause
+- Select: options
+- Up: Map
+- Down: Journal
+- Left: Toggle AI
+- Right: Character Arbitration
+- Left Stick: Move cursor
+- Right Stick: Move screen
+
+To enter text: press Start + Right, up and down selects the letter, left and right moves forwards and backwards. Start or A to get out.
 
 ## Required libs
 
@@ -26,11 +44,11 @@ Use portmaster to install gemrb, copy the desired game or games into `{PORTFOLDE
 
     cd build
 
-    cmake .. -DOPENGL_BACKEND=GLES -DCMAKE_BUILD_TYPE=Debug -DLAYOUT=home  -DUSE_ICONV=OFF -DDISABLE_VIDEOCORE=ON -DUSE_LIBVLC=OFF -DCMAKE_INSTALL_PREFIX:FILE="engine"
+    cmake .. -DOPENGL_BACKEND=GLES -DCMAKE_BUILD_TYPE=Release -DLAYOUT=home  -DUSE_ICONV=OFF -DDISABLE_VIDEOCORE=ON -DUSE_LIBVLC=OFF -DCMAKE_INSTALL_PREFIX:FILE="engine"
 
     cd engine
 
-    zip -vr ../engine.zip engine/
+    zip -9r ../engine.zip engine/
 
     cd ..
 
@@ -38,21 +56,19 @@ At the end, you want the `engine.zip` file is what you want.
 
 # Game folder structure
 
-- gemrb/games/GAME_NAME
-  - INSTALLED GAME FILES
-  - CD1/
-    - CD 1 FILES
-  - CD2/
-    - CD 2 FILES
-  - CD3/
-    - CD 3 FILES
-  - CD4/
-    - CD 4 FILES
+- gemrb/games/GAME_ID
+ - **bg1** for Baldur's Gate 1
+ - **bg2** for Baldur's Gate 2
+ - **iwd** for Icewind Dale
+ - **iwd2** for Icewind Dale 2
+ - **pst** for Planescape Torment
 
 # TODO:
 
-- [ ] Get a game to work!
-
+- [x] Get a game to work!
+- [ ] Add custom configs where needed
+- [ ] Add custom gptokeyb mappings if needed
+- [ ] Fix flickering cursor
 
 # Thanks
 
